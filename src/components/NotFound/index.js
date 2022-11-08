7e4719cbd2822d88f1f67f12c491c4331bff8294
AdharshReddyC <adharshreddy.c@gmail.com>
2022-11-08 09:01:03 +0000

    book hum mini proect

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <h1></h1>
+    <img
+      src="https://res.cloudinary.com/dftcpr6nl/image/upload/v1667201882/page_not_found_no2zdg.png"
+      alt="not found"
+      className="not-found-img"
+    />
+  </div>
+)
+
+export default NotFound
